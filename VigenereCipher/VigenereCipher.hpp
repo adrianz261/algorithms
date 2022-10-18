7628041187a36150e868c08e5e4cdeea103357be
adrianz261 <me@Adrian.localdomain>
2022-10-18 19:09:16 +0200

    First commit.

---
+++ VigenereCipher/VigenereCipher.hpp
@@ -0,0 +1,8 @@
+#ifndef VIGENERE
+#define VIGENERE
+
+#include <string>
+
+std::string vigenere(const std::string &input, const std::string &key);
+
+#endif
---
+++ VigenereCipher/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "VigenereCipher.hpp"
+
+int main()
+{
+    return 0;
+}
